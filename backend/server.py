025c329b6728ef6f369a62a60cb5f68921fe2490
Aryansh2806 <aryansharma28.in@gmail.com>
2026-04-10 06:33:07 +0530

    commit

--- backend/server.py
+++ backend/server.py
@@ -3798,7 +3798,7 @@ app.include_router(api_router)
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=["*"],
+    allow_origins=["https://wms-sael-new.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
